3cc542b98a00baccfa399393c7d6eb55a6bfc627
joe-rochette <joe.rochette@chubb.com>
2018-05-03 23:18:05 -0500

    fixed description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject jdbc-data-mover/lein-template "0.0.1-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Leiningen template to start projects utilizing JDBC, and core.async for database movement."
+  :url "https://github.com/joefromct/jdbc-data-mover-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
