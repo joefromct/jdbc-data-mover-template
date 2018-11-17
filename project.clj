4c2786f047cdd1ff05414915f75f26e1f37f8c17
joe-rochette <joe.rochette@chubb.com>
2018-11-16 22:50:13 -0500

    deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jdbc-data-mover/lein-template "0.0.1-SNAPSHOT"
+(defproject jdbc-data-mover/lein-template "0.0.1"
   :description "Leiningen template to start projects utilizing JDBC, and core.async for database movement."
   :url "https://github.com/joefromct/jdbc-data-mover-template"
   :license {:name "Eclipse Public License"
